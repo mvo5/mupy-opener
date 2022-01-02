f71e24964064de77423bd2647eedcb461b50b321
Michael Vogt <mvo@ubuntu.com>
2022-01-02 19:37:39 +0100

    open: fix open to use new "read_config()" and inlcude device-info

--- cli/open.py
+++ cli/open.py
@@ -6,7 +6,7 @@ from main import (
     send_with_hmac,
     recv_with_hmac,
 )
-from boot import read_config
+from config import read_config
 from sjm import SignedJsonMessage
 
 HOST = "opener"
@@ -16,9 +16,10 @@ PORT = 8877
 def open():
     cfg = read_config()
     key = cfg["hmac-key"].encode("ascii")
+    hostname = cfg["hostname"]
 
     so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    so.connect((HOST, PORT))
+    so.connect((hostname, PORT))
     so = so.makefile("rwb", 0)
     m1 = so.readline()
     # print(m1)
@@ -27,7 +28,9 @@ def open():
     if sjm.payload != {"api": "opener", "version": 1}:
         raise Exception("unexpected payload %s" % str(sjm))
     # send open command
-    send_with_hmac(so, key, sjm.nonce, {"cmd": "open"})
+    device_info = socket.gethostname()
+    cmd_json = {"cmd": "open", "device-info": device_info}
+    send_with_hmac(so, key, sjm.nonce, cmd_json)
     msg = recv_with_hmac(so, key, sjm.nonce)
     if msg["status"] != "ok":
         raise Exception("unknown status in %s" % msg)
