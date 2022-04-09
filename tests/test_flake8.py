a787e08c0de0ca23d5fa0b8e9e78d0d323d584c0
Michael Vogt <mvo@ubuntu.com>
2022-04-09 19:26:15 +0200

    tests: add new test_static.py that also tests for black formated code

--- tests/test_flake8.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-import unittest
-
-
-class TestFlake8(unittest.TestCase):
-    def test_flake8(self):
-        p = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
-        subprocess.check_call(["flake8", "--ignore=E402", "--exclude=./lib", p])
---
+++ tests/test_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import os
+import os.path
+import subprocess
+import unittest
+
+
+class TestStatic(unittest.TestCase):
+    def setUp(self):
+        self.prj_path = os.path.join(os.path.dirname(__file__), "..")
+
+    def test_flake8(self):
+        subprocess.check_call(
+            ["flake8", "--ignore=E402", "--exclude=./lib", self.prj_path]
+        )
+
+    def test_black_clean(self):
+        subprocess.check_call(["black", "--check", self.prj_path])
