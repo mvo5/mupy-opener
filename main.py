97406787129d316df4780a55bb110f6adde13273
Michael Vogt <mvo@ubuntu.com>
2022-09-18 21:50:41 +0200

    main: discard empty readline() data

--- main.py
+++ main.py
@@ -66,8 +66,17 @@ def send_with_hmac(so, key, nonce, msg):
     so.write(str(sjm).encode("utf-8") + b"\n")
 
 
+def readline_until_non_empty(so):
+    while True:
+        msg = so.readline()
+        if msg:
+            return msg
+
+
 def recv_with_hmac(so, key, nonce):
-    msg = so.readline()
+    # sometimes we get an empty readline message here, discard that and
+    # read until there is actual data
+    msg = readline_until_non_empty(so)
     sjm = SignedJsonMessage.from_string(msg.decode("utf-8"), key, nonce)
     return sjm.payload
 
